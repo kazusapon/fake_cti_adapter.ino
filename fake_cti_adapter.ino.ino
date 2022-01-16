b81ebedeb404f16233160e0b0cc2575eca2fea0e
kazusapon <flum1278pool@gmail.com>
2022-01-16 23:41:10 +0900

    未テストだけどもコミットじゃ

--- fake_cti_adapter.ino.ino
+++ fake_cti_adapter.ino.ino
@@ -1,9 +1,18 @@
 void setup() {
   // put your setup code here, to run once:
-  
+  initSerial();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  
+  int data = 1234567890;
+  Serial.print(data);
+  Serial.flush();
+  Serial.write("\n");
+
+  delay(3000);
+}
+
+void initSerial() {
+  Serial.begin(9600);
 }
